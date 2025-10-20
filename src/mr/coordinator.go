a838e9b326a0a87faa595c3720cb8283d01285ab
mlinku <mlinku39@gmail.com>
2025-10-20 21:43:12 +0800

    了解初步框架，实现lab1的worker初始化和初步任务分配

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,50 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
 type Coordinator struct {
 	// Your definitions here.
 
+	// mantain a counter for worker IDs, initialize to 0
+	workerIDCounter int
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
+	fmt.Println("Example RPC called with args:", args.X)
 	return nil
 }
 
+// init worker RPC handler
+func (c *Coordinator) InitWorker(args *WorkerArgs, reply *InitWorkerReply) error {
+	reply.WorkerID = c.workerIDCounter
+	c.workerIDCounter++
+	fmt.Println("Init Worker RPC called with WorkerID:", reply.WorkerID)
+	return nil
+}
 
-//
+func (c *Coordinator) AssignTask(arg *WorkerArgs, reply *AssignTaskReply) error {
+	// temp implementation for testing
+	reply.WorkerID = arg.WorkerID
+	reply.TaskType = TaskMap
+	reply.TaskFile = "pg-being_ernest.txt"
+	fmt.Printf("Assign Task RPC called for WorkerID %v: assigned Map task for file %v\n", reply.WorkerID, reply.TaskFile)
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +58,23 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
+	c.workerIDCounter = 0
 	// Your code here.
-
 
 	c.server()
 	return &c
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,13 +6,13 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
-//
 // example to show how to declare the arguments
 // and reply for an RPC.
-//
 
 type ExampleArgs struct {
 	X int
@@ -22,8 +22,40 @@ type ExampleReply struct {
 	Y int
 }
 
-// Add your RPC definitions here.
+// worker_rpc_definition
+type WorkerArgs struct {
+	WorkerID int
+	CallType CallType // 0: init 1: assign task 2: report task
+}
 
+type InitWorkerReply struct {
+	WorkerID int
+}
+
+type AssignTaskReply struct {
+	WorkerID int
+	TaskType TaskType //  0: Map 1: Reduce 2: Wait 3: Exit
+	TaskFile string   // 仅 Map 时有效
+}
+
+type CallType int
+
+const (
+	CallInit CallType = iota
+	CallAssignTask
+	CallReportTask
+)
+
+type TaskType int
+
+const (
+	TaskMap TaskType = iota
+	TaskReduce
+	TaskWait
+	TaskExit
+)
+
+// Add your RPC definitions here.
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
