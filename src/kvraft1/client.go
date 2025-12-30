b37e9ead990755ba0b39631fbe0c3aec7a919b2a
mlinku <1203440489@qq.com>
2025-12-30 15:41:50 +0000

    4B 引用lab2的verison机制

--- src/kvraft1/client.go
+++ src/kvraft1/client.go
@@ -1,20 +1,24 @@
 package kvraft
 
 import (
-	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
-)
+	"sync"
+	"time"
 
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
+)
 
 type Clerk struct {
 	clnt    *tester.Clnt
 	servers []string
 	// You will have to modify this struct.
+	leaderId int
+	mu       sync.Mutex
 }
 
 func MakeClerk(clnt *tester.Clnt, servers []string) kvtest.IKVClerk {
-	ck := &Clerk{clnt: clnt, servers: servers}
+	ck := &Clerk{clnt: clnt, servers: servers, leaderId: 0, mu: sync.Mutex{}}
 	// You'll have to add code here.
 	return ck
 }
@@ -30,9 +34,36 @@ func MakeClerk(clnt *tester.Clnt, servers []string) kvtest.IKVClerk {
 // must match the declared types of the RPC handler function's
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
-
 	// You will have to modify this function.
-	return "", 0, ""
+	args := rpc.GetArgs{}
+	args.Key = key
+	serverId := ck.servers[ck.leaderId]
+	for {
+		reply := rpc.GetReply{}
+		ok := ck.clnt.Call(serverId, "KVServer.Get", &args, &reply)
+		if ok {
+			switch reply.Err {
+			case rpc.OK:
+				return reply.Value, reply.Version, reply.Err
+			case rpc.ErrNoKey:
+				return "", 0, rpc.ErrNoKey
+			case rpc.ErrWrongLeader:
+				ck.mu.Lock()
+				ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+				serverId = ck.servers[ck.leaderId]
+				ck.mu.Unlock()
+				continue
+			default:
+				return "", 0, reply.Err
+			}
+		}
+		// rpc 失败，尝试下一个 server
+		ck.mu.Lock()
+		ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+		serverId = ck.servers[ck.leaderId]
+		ck.mu.Unlock()
+		time.Sleep(2 * time.Millisecond)
+	}
 }
 
 // Put updates key with value only if the version in the
@@ -54,5 +85,40 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	return ""
+	args := rpc.PutArgs{}
+	args.Key = key
+	args.Value = value
+	args.Version = version
+	serverId := ck.servers[ck.leaderId]
+	times := 0
+	for {
+		reply := rpc.PutReply{}
+		ok := ck.clnt.Call(serverId, "KVServer.Put", &args, &reply)
+		times += 1
+		if ok {
+			switch reply.Err {
+			case rpc.OK:
+				return rpc.OK
+			case rpc.ErrWrongLeader:
+				ck.mu.Lock()
+				ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+				serverId = ck.servers[ck.leaderId]
+				ck.mu.Unlock()
+				continue
+			case rpc.ErrVersion:
+				if times == 1 {
+					return rpc.ErrVersion
+				}
+				return rpc.ErrMaybe
+			default:
+				return reply.Err
+			}
+		}
+		// rpc 失败，尝试下一个 server
+		ck.mu.Lock()
+		ck.leaderId = (ck.leaderId + 1) % len(ck.servers)
+		serverId = ck.servers[ck.leaderId]
+		ck.mu.Unlock()
+		time.Sleep(2 * time.Millisecond)
+	}
 }
--- src/kvraft1/server.go
+++ src/kvraft1/server.go
@@ -1,14 +1,14 @@
 package kvraft
 
 import (
+	"sync"
 	"sync/atomic"
 
 	"6.5840/kvraft1/rsm"
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labgob"
 	"6.5840/labrpc"
-	"6.5840/tester1"
-
+	tester "6.5840/tester1"
 )
 
 type KVServer struct {
@@ -17,6 +17,21 @@ type KVServer struct {
 	rsm  *rsm.RSM
 
 	// Your definitions here.
+	mu       sync.Mutex
+	kv_store map[string]kvEntry
+}
+
+type kvEntry struct {
+	Value   string
+	Version rpc.Tversion
+}
+
+type Req struct {
+	// Your definitions here.
+	ReqType int // 0 for Get, 1 for Put
+	Key     string
+	Value   string
+	Version rpc.Tversion
 }
 
 // To type-cast req to the right type, take a look at Go's type switches or type
@@ -26,6 +41,47 @@ type KVServer struct {
 // https://go.dev/tour/methods/15
 func (kv *KVServer) DoOp(req any) any {
 	// Your code here
+	if r, ok := req.(Req); ok {
+		switch r.ReqType {
+		case 0: // Get
+			kv.mu.Lock()
+			defer kv.mu.Unlock()
+			entry, exists := kv.kv_store[r.Key]
+			if exists {
+				return rpc.GetReply{Err: rpc.OK, Value: entry.Value, Version: entry.Version}
+			} else {
+				return rpc.GetReply{Err: rpc.ErrNoKey}
+			}
+		case 1: // Put
+			kv.mu.Lock()
+			defer kv.mu.Unlock()
+			entry, exists := kv.kv_store[r.Key]
+			if exists {
+				// key exists
+				if r.Version != entry.Version {
+					// version mismatch
+					return rpc.PutReply{Err: rpc.ErrVersion}
+				} else {
+					// version match, update value and increment version
+					entry.Value = r.Value
+					entry.Version++
+					kv.kv_store[r.Key] = entry
+					return rpc.PutReply{Err: rpc.OK}
+				}
+			} else {
+				// key does not exist
+				if r.Version != 0 {
+					// version mismatch
+					return rpc.PutReply{Err: rpc.ErrNoKey}
+				} else {
+					// install new key with version 1
+					kv.kv_store[r.Key] = kvEntry{Value: r.Value, Version: 1}
+					return rpc.PutReply{Err: rpc.OK}
+				}
+			}
+		}
+		return nil
+	}
 	return nil
 }
 
@@ -42,12 +98,44 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here. Use kv.rsm.Submit() to submit args
 	// You can use go's type casts to turn the any return value
 	// of Submit() into a GetReply: rep.(rpc.GetReply)
+	req := Req{
+		ReqType: 0,
+		Key:     args.Key,
+	}
+	err, value := kv.rsm.Submit(req)
+	if err != rpc.OK {
+		reply.Err = err
+		return
+	}
+	if entry, ok := value.(rpc.GetReply); ok {
+		reply.Err = entry.Err
+		reply.Value = entry.Value
+		reply.Version = entry.Version
+	}
+
 }
 
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here. Use kv.rsm.Submit() to submit args
 	// You can use go's type casts to turn the any return value
 	// of Submit() into a PutReply: rep.(rpc.PutReply)
+	req := Req{
+		ReqType: 1,
+		Key:     args.Key,
+		Value:   args.Value,
+		Version: args.Version,
+		// 记得之后加上 ClientId 和 RequestId
+	}
+	err, value := kv.rsm.Submit(req)
+	if err != rpc.OK {
+		reply.Err = err
+		return
+	}
+	if putReply, ok := value.(rpc.PutReply); ok {
+		reply.Err = putReply.Err
+		return
+	}
+
 }
 
 // the tester calls Kill() when a KVServer instance won't
@@ -76,9 +164,9 @@ func StartKVServer(servers []*labrpc.ClientEnd, gid tester.Tgid, me int, persist
 	labgob.Register(rsm.Op{})
 	labgob.Register(rpc.PutArgs{})
 	labgob.Register(rpc.GetArgs{})
+	labgob.Register(Req{})
 
-	kv := &KVServer{me: me}
-
+	kv := &KVServer{me: me, mu: sync.Mutex{}, kv_store: make(map[string]kvEntry)}
 
 	kv.rsm = rsm.MakeRSM(servers, me, persister, maxraftstate, kv)
 	// You may need initialization code here.
